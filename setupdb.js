bb684819cfdace1a0c20f26ce953a726ee8ae930
junorouse <junorouse@gmail.com>
2016-12-08 00:53:30 +0900

    Add visit counter

--- setupdb.js
+++ setupdb.js
@@ -5,24 +5,12 @@ const db = new sqlite3.Database('db.sqlite3');
 db.serialize(function() {
   db.run("CREATE TABLE valid_xss_checker(\
            rId integer primary key   autoincrement,\
+           count unsigned integer default 0,\
            url           TEXT      NOT NULL,\
            form_data            text       NOT NULL,\
            t        TIMESTAMP\
            DEFAULT CURRENT_TIMESTAMP\
   );");
-
-  /*
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Isadfpsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-  */
-
 });
 
 db.close();
