367e7a54172d91d4bf94d504483bcaf95a849f9a
junorouse <junorouse@gmail.com>
2016-12-08 09:49:25 +0900

    Add dev mode

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
 
 const app = express();
 
 let port = 8080;
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+
+    const config = require('../webpack.dev.config');
+    let compiler = webpack(config);
+    let devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(devPort, () => {
+        console.log('webpack-dev-server is listening on port', port);
+    });
+}
 
 let bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
